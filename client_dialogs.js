dd66436a09f8446cc5c4015f8531fc683ee34e97
behai-nguyen <behai_nguyen@hotmail.com>
2022-11-03 14:39:56 +1100

    #message renamed to #bodyText.

--- client_dialogs.js
+++ client_dialogs.js
@@ -1,54 +1,54 @@
 /*
-    Date Created: 04/04/2022.
-	
-    Generic dialogs which can be called from HTML pages.
-	
-    Required "bootstrap_dialogs.js".	
+Generic dialogs which can be called from HTML pages.
+
+Dependencies:
+
+1. D:\Codes\WebWork\js\bootstrap_dialogs.js
 */
 
 function displayError( xhr, error, errorThrown ) {
-	var dialog = new GenericDialog( { 
-	   dialogId: '#popupDialog', 
+	var dialog = new GenericDialog( {
+	   dialogId: '#popupDialog',
 	   title: 'Oops something\' happened...',
-	   message: `<span>Please contact support with message <strong>${errorThrown}</strong>.</span>` } );
-	   
+	   bodyText: `<span>Please contact support with message <strong>${errorThrown}</strong>.</span>` } );
+
 	dialog.open();
 };
 
 function displayError1( errorThrown ) {
-	var dialog = new GenericDialog( { 
-	   dialogId: '#popupDialog', 
+	var dialog = new GenericDialog( {
+	   dialogId: '#popupDialog',
 	   title: 'For your info...',
-	   message: `<span><strong>${errorThrown}</strong>.</span>` } );
-	   
+	   bodyText: `<span><strong>${errorThrown}</strong>.</span>` } );
+
 	dialog.open();
 };
 
 function displayWarning( xhr, error, errorThrown ) {
-	var dialog = new GenericDialog( { 
-	   dialogId: '#popupDialog', 
-	   title: 'For your info...', 
-	   message: `<span>Please contact support with message <strong>${errorThrown}</strong>.</span>`, 
+	var dialog = new GenericDialog( {
+	   dialogId: '#popupDialog',
+	   title: 'For your info...',
+	   bodyText: `<span>Please contact support with message <strong>${errorThrown}</strong>.</span>`,
 	   buttonClass: 'btn-warning' } );
-	   
-	dialog.open();	
+
+	dialog.open();
 };
 
 function displayInfo( msg ) {
-	var dialog = new GenericDialog( { 
-	   dialogId: '#popupDialog', 
+	var dialog = new GenericDialog( {
+	   dialogId: '#popupDialog',
 	   title: 'For your info...',
-	   message: `<span>${msg}</span>`,
+	   bodyText: `<span>${msg}</span>`,
 	   buttonClass: 'btn-info' } );
-	   
-	dialog.open();				   
+
+	dialog.open();
 };
 
 function parsleyValidationError( errorInHtml ) {
-	var dialog = new GenericDialog( { 
-	   dialogId: '#popupDialog', 
-	   title: 'Please enter valid data', 
-	   message: errorInHtml } );
-	   
-	dialog.open();				   
+	var dialog = new GenericDialog( {
+	   dialogId: '#popupDialog',
+	   title: 'Please enter valid data',
+	   bodyText: errorInHtml } );
+
+	dialog.open();
 };
