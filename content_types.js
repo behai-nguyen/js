63a8c07c6665c253260c628cdb6831d8123af459
behai-nguyen <behai_nguyen@hotmail.com>
2023-06-04 11:08:32 +1000

    Media types / content types used by applications and not strictly adhere to IANA.

---
+++ content_types.js
@@ -0,0 +1,12 @@
+/*
+Date Created: 15/02/2023.
+
+Dependencies: None.
+	
+Valid media types / content types are supposed to be registered with the IANA:
+    https://www.iana.org/assignments/media-types/media-types.xhtml
+	
+This module defines those used by applications and not strictly adhere to IANA.
+*/
+
+X_WWW_FORM_URLENCODED_UTF8 = 'application/x-www-form-urlencoded; charset=UTF-8';
