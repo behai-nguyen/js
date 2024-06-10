c49e8ba9651e6b4453bf9c9e5573dd8ec25d084c
behai-nguyen <behai_nguyen@hotmail.com>
2024-06-10 12:38:07 +1000

    Added constants from Rust mine crate version 0.3.17.

--- content_types.js
+++ content_types.js
@@ -9,4 +9,44 @@ Valid media types / content types are supposed to be registered with the IANA:
 This module defines those used by applications and not strictly adhere to IANA.
 */
 
-X_WWW_FORM_URLENCODED_UTF8 = 'application/x-www-form-urlencoded; charset=UTF-8';
+X_WWW_FORM_URLENCODED_UTF8 = "application/x-www-form-urlencoded; charset=UTF-8";
+
+/* Rust mine crate version 0.3.17. */
+
+STAR_STAR = "*//*";
+
+TEXT_STAR = "text/*";
+TEXT_PLAIN = "text/plain";
+TEXT_PLAIN_UTF_8 = "text/plain; charset=utf-8";
+TEXT_HTML = "text/html";
+TEXT_HTML_UTF_8 = "text/html; charset=utf-8";
+TEXT_CSS = "text/css";
+TEXT_CSS_UTF_8 = "text/css; charset=utf-8";
+TEXT_JAVASCRIPT = "text/javascript";
+TEXT_XML = "text/xml";
+TEXT_EVENT_STREAM = "text/event-stream";
+TEXT_CSV = "text/csv";
+TEXT_CSV_UTF_8 = "text/csv; charset=utf-8";
+TEXT_TAB_SEPARATED_VALUES = "text/tab-separated-values";
+TEXT_TAB_SEPARATED_VALUES_UTF_8 = "text/tab-separated-values; charset=utf-8";
+TEXT_VCARD = "text/vcard";
+
+IMAGE_STAR = "image/*";
+IMAGE_JPEG = "image/jpeg";
+IMAGE_GIF = "image/gif";
+IMAGE_PNG = "image/png";
+IMAGE_BMP = "image/bmp";
+IMAGE_SVG = "image/svg+xml";
+
+FONT_WOFF = "font/woff";
+FONT_WOFF2 = "font/woff2";
+
+APPLICATION_JSON = "application/json";
+APPLICATION_JAVASCRIPT = "application/javascript";
+APPLICATION_JAVASCRIPT_UTF_8 = "application/javascript; charset=utf-8";
+APPLICATION_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded";
+APPLICATION_OCTET_STREAM = "application/octet-stream";
+APPLICATION_MSGPACK = "application/msgpack";
+APPLICATION_PDF = "application/pdf";
+
+MULTIPART_FORM_DATA = "multipart/form-data";
